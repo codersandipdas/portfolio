57f9f9407997101328d65d4fc39da3c36cce2cf2
Sandip Das <codersandipdas@gmail.com>
2024-09-21 16:35:40 +0530

    made code winow header filename centered

--- src/components/codeWindow/CodeWindow.tsx
+++ src/components/codeWindow/CodeWindow.tsx
@@ -31,7 +31,7 @@ export const CodeWindow = () => {
   return (
     <div className='relative w-full h-full overflow-hidden rounded-md border-2 bg-gray-900 p-2.5 shadow-2xl drop-shadow-lg border-gray-600/40'>
       <div className='mb-2 flex items-center border-b border-b-gray-800 pb-2'>
-        <div className='shrink-0 flex gap-2 items-center'>
+        <div className='shrink-0 flex gap-2 items-center min-w-[60px]'>
           <button className='h-3 w-3 rounded-full bg-gray-700 transition-colors hover:bg-[#d85146]' />
           <button className='h-3 w-3 rounded-full bg-gray-700 transition-colors hover:bg-[#F4BF50]' />
           <button className='h-3 w-3 rounded-full bg-gray-700 transition-colors hover:bg-[#61C454]' />
@@ -39,7 +39,7 @@ export const CodeWindow = () => {
         <div className='flex-1  select-none text-center text-[13px] leading-none tracking-wide text-gray-500'>
           /index.tsx
         </div>
-        <div className='shrink-0 flex gap-2 items-center'>
+        <div className='shrink-0 flex gap-2 items-center min-w-[60px] justify-end'>
           <button className='text-gray-500 hover:text-white h-5 w-5 transition-colors'>
             <span className='sr-only'>Copy</span>
             <FaCopy />
