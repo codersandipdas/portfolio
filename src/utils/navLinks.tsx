271919c00f3822b6e662e5af4756f993aef73124
Sandip Das <codersandipdas@gmail.com>
2024-09-22 20:46:09 +0530

    removed resources page

--- src/utils/navLinks.tsx
+++ src/utils/navLinks.tsx
@@ -1,7 +1,6 @@
 export const links = [
   { id: '0', title: 'Home', link: '/' },
   { id: '1', title: 'Portfolio', link: '/portfolios' },
-  // { id: '2', title: 'Resources', link: '/resources' },
   { id: '3', title: 'Blog', link: '/blogs' },
   // { id: '4', title: 'Contact', link: '/contact' },
 ];
