b2e49d184c299e352fd308c4f403bc3a73860f94
Sandip Das <codersandipdas@gmail.com>
2024-09-22 20:31:50 +0530

    updated nav links

--- src/utils/navLinks.tsx
+++ src/utils/navLinks.tsx
@@ -1,6 +1,7 @@
 export const links = [
+  { id: '0', title: 'Home', link: '/' },
   { id: '1', title: 'Portfolio', link: '/portfolios' },
-  { id: '2', title: 'Resources', link: '/resources' },
+  // { id: '2', title: 'Resources', link: '/resources' },
   { id: '3', title: 'Blog', link: '/blogs' },
   // { id: '4', title: 'Contact', link: '/contact' },
 ];
